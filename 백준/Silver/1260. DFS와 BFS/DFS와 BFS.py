78abe7ce393fbaf3d74a5a0661f5b1d0ff2b66a6
이원석 <wonseok2877@gmail.com>
2023-12-08 21:01:11 +0900

    [Silver II] Title: DFS와 BFS, Time: 88 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,65 @@
+# DFS BFS 기본 문제
+
+import sys
+from collections import deque
+
+read = sys.stdin.readline
+
+# 입력
+N, M, V = map(int, read().split())
+
+# 그래프 정의
+adj_list = [[] for _ in range(N + 1)]
+visited = [False for _ in range(N + 1)]
+stack = deque()
+queue = deque()
+
+# 간선 입력
+for _ in range(M):
+    x, y = map(int, read().split())
+    adj_list[x].append(y)
+    adj_list[y].append(x)
+
+# 오름차순으로 인접 노드 방문하기 위한 정렬
+for i in range(N + 1):
+    adj_list[i].sort(reverse=True)
+
+
+# DFS(V)
+stack.append(V)
+visited[V] = True
+print(V, end=' ')
+
+while stack:
+    node = stack.pop()
+
+    if not visited[node]:
+        visited[node] = True  # 방문 표시
+        print(node, end=' ')
+
+    for adj in adj_list[node]:  # node의 인접 노드들에 대해
+        if not visited[adj]:
+            stack.append(adj)
+
+print()
+
+visited = [False for _ in range(N + 1)]  # 방문 기록 초기화
+for i in range(N + 1):
+    adj_list[i].sort()
+
+# BFS
+# queue가 비지 않는 동안
+# 1. node <- dequeue
+# 2. 인접한 노드들을 모두 enqueue
+queue.appendleft(V)
+visited[V] = True
+
+while queue:
+    node = queue.pop()
+    print(node, end=' ')
+
+    for adj in adj_list[node]:
+        if not visited[adj]:
+            queue.appendleft(adj)
+            visited[adj] = True
+
