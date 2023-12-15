e9ed84be1ebfe553f4e7b16a6dfbe6045270cb67
이원석 <wonseok2877@gmail.com>
2023-12-15 12:23:41 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -5,7 +5,7 @@
 # 모든 경우의 수를 다 구하면 O(N^2)으로 시간 초과.
 # 반복문 1번으로 풀 수밖에 없음. 그 안에서 max값을 구해야 함
 # 이전까지의 합이 음수일 경우, 무조건 현재 원소만 취해야 함. 그 반대일 경우, 현재 원소와 더해야 함
-# 이전까지의 합( d[i] ) + 현재 원소 vs 현재 원소 
+# 이전까지의 합( d[i] ) + 현재 원소 vs 현재 원소
 # 점화식: d[i] = max(input[i], d[i-1] + input[i])
 import sys
 
@@ -22,10 +22,13 @@ d[0] = input_list[0]
 # main
 for i in range(1, N):
     # 현재 원소 vs 이전까지의 합 + 현재 원소
-    d[i] = max(input_list[i], d[i - 1] + input_list[i])
+    if d[i - 1] < 0:
+        d[i] = input_list[i]
+    else:
+        d[i] = d[i - 1] + input_list[i]
+    # d[i] = max(input_list[i], d[i - 1] + input_list[i])
 
 print(max(d))
-
 
 # O(N) 그리디
 # cur = 0
